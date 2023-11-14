3b09e3a22918851276236ad55761cf43aa0dd54b
abelnhm <abelnhm92@gmail.com>
2023-11-14 19:08:39 +0100

    Add redux with load

--- src/components/list/list.tsx
+++ src/components/list/list.tsx
@@ -1,16 +1,9 @@
-import { useCallback, useEffect, useMemo, useState } from 'react';
-import { Character } from '../../model/characters';
+import { useEffect } from 'react';
 import { Card } from '../card/card';
-import { ApiRepo } from '../../services/api.repo';
+import { useCharacters } from '../hook/use.characters';
 
 export function List() {
-  const [characters, setCharacters] = useState<Character[]>([]);
-  const repo = useMemo(() => new ApiRepo(), []);
-
-  const loadCharacters = useCallback(async () => {
-    const loadedCharacters: Character = await repo.getCharacters();
-    setCharacters([loadedCharacters]);
-  }, [repo]);
+  const { characters, loadCharacters } = useCharacters();
 
   useEffect(() => {
     loadCharacters();
